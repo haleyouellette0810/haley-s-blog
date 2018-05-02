59676c7ea1e5f53593fd17bb54fbc2f0d74a8769
haleyouellette0810 <36276282+haleyouellette0810@users.noreply.github.com>
2018-05-02 16:37:46 -0700

    setting up fb

---
+++ main.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/4.13.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyArJy5YjFgtj6tf9aTWc3WOHkEh8piGsko",
+    authDomain: "haley-s-blog.firebaseapp.com",
+    databaseURL: "https://haley-s-blog.firebaseio.com",
+    projectId: "haley-s-blog",
+    storageBucket: "",
+    messagingSenderId: "798556566486"
+  };
+  firebase.initializeApp(config);
+</script>
